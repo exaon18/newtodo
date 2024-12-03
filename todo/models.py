1d05108749d20f2990650f013014cac743953e69
exaon18 <exaontech@gamil.com>
2024-12-03 09:02:46 -0800

    added date and time

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    date=models.DateTimeField(auto_now_add=True ,blank=True)
